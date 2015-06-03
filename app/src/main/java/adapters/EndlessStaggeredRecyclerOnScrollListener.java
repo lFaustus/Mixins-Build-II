f99ec13201d9776c7c23ded32abdcc3bc0069395
lFaustus <Fluxinated@hotmail.com>
2015-06-03 19:29:25 +0800

    Update 06-03-15 5:34PM **Recommit -Added Infinite Horizontal Scrolling -Fixes on Tile Arrangements

---
+++ app/src/main/java/adapters/EndlessStaggeredRecyclerOnScrollListener.java
@@ -0,0 +1,48 @@
+package adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.Log;
+
+/**
+ * Created by flux on 6/1/15.
+ */
+public abstract class EndlessStaggeredRecyclerOnScrollListener extends RecyclerView.OnScrollListener
+{
+    private StaggeredGridLayoutManager mStaggeredgrid;
+    private int firstVisibleItem,VisibleItemCount,totalItemCount,current_page = 1;
+    private final int  visibleThreshold = 5;
+
+    public EndlessStaggeredRecyclerOnScrollListener(StaggeredGridLayoutManager staggeredgrid)
+    {
+        mStaggeredgrid = staggeredgrid;
+    }
+
+    @Override
+    public void onScrolled(RecyclerView recyclerView, int dx, int dy)
+    {
+        super.onScrolled(recyclerView, dx, dy);
+        firstVisibleItem = mStaggeredgrid.findFirstVisibleItemPositions(null)[0];
+        VisibleItemCount = recyclerView.getChildCount();
+        totalItemCount = mStaggeredgrid.getItemCount();
+        current_page = totalItemCount;
+        Log.i("ItemCount", "ItemCount " + totalItemCount);
+        Log.i("Childcount","childcount: "+VisibleItemCount);
+        Log.i("FirstPosition", "FirstPosition: " + firstVisibleItem);
+
+        if((totalItemCount - VisibleItemCount) <= (firstVisibleItem + visibleThreshold))
+        {
+            current_page ++;
+            OnLoadMore(current_page);
+        }
+        /*if((VisibleItemCount + firstVisibleItem) >= totalItemCount)
+        {
+            OnLoadMore(current_page);
+            current_page ++;
+        }*/
+
+
+    }
+
+    public abstract void OnLoadMore(int page);
+}
---
+++ app/src/main/java/database/GenerateLiquors.java
@@ -0,0 +1,150 @@
+package database;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import model.Liquor;
+
+/**
+ * Created by flux on 6/1/15.
+ */
+public class GenerateLiquors
+{
+    private static int mLastTileType = -1;
+    public static final int SMALL_TILE = 0;
+    public static final int LONG_TILE = 1;
+    public static final int BIG_TILE = 2;
+    private static ArrayList<Liquor> liquorlist = null;
+    private static boolean BigTile = false;
+    private static int temp = -1;
+    private static String[] materialPalette;
+
+    public static ArrayList<Liquor> generateDrinks(int number)
+    {
+
+        LoadDrinks(number,liquorlist);
+        return liquorlist;
+    }
+
+    /*public static void LoadMoreDrinks(int number,ArrayList<Liquor> liquors)
+    {
+       tileGenerator(number,liquors);
+
+    }*/
+
+    public static void setMaterialPalette(java.lang.String[] materialPalettes)
+    {
+        materialPalette = materialPalettes;
+    }
+
+    public static void LoadDrinks(int number,ArrayList<Liquor> liquors )
+    {
+
+        int i;
+
+        if(liquors == null)
+        {
+            liquors = new ArrayList<>();
+            liquorlist = liquors;
+            i = 0;
+
+
+        }
+        else
+            i = liquors.size();
+
+        for(; i<number; i++)
+        {
+
+            if (mLastTileType == -1 || temp == BIG_TILE)
+            {
+                do
+                {
+                    temp = randomTile(3);
+
+                } while ((mLastTileType == temp && i!=0) || mLastTileType == temp);
+                mLastTileType = temp;
+
+            }
+
+
+
+            if(mLastTileType == SMALL_TILE)
+            {
+                if(BigTile)
+                {
+                    liquors.add(new Liquor("Liquor Number: " + i, mLastTileType,randomTileColor()));
+                    mLastTileType = BIG_TILE;
+                    //temp = mLastTileType;
+                }
+                else
+                {
+                    if (temp == LONG_TILE)
+                    {
+                        temp = mLastTileType;
+                        mLastTileType = SMALL_TILE;
+                        liquors.add(new Liquor("Liquor Number: " + i, mLastTileType,randomTileColor()));
+                        BigTile = true;
+                    } else
+                    {
+                            liquors.add(new Liquor("Liquor Number: " + i, mLastTileType,randomTileColor()));
+                            temp = SMALL_TILE;
+                            mLastTileType = LONG_TILE;
+
+                    }
+
+
+                }
+            }
+            else
+            {
+                if (mLastTileType == LONG_TILE)
+                {
+                    if (temp == SMALL_TILE)
+                    {
+                        liquors.add(new Liquor("Liquor Number: " + i, mLastTileType,randomTileColor()));
+                        mLastTileType = SMALL_TILE;
+                        BigTile = true;
+                    } else
+                    {
+                        liquors.add(new Liquor("Liquor Number: " + i, mLastTileType,randomTileColor()));
+                        temp = LONG_TILE;
+                        mLastTileType = SMALL_TILE;
+
+                    }
+                }
+                else
+                {
+                    temp = mLastTileType;
+                    liquors.add(new Liquor("Liquor Number: " + i, mLastTileType,randomTileColor()));
+                    BigTile = false;
+                   /* do
+                    {
+                        temp = randomTile(2);
+
+                    } while (mLastTileType == temp);*/
+                    //mLastTileType = temp;
+                }
+            }
+
+        }
+    }
+
+    private static int randomTile(int number)
+    {
+        Random rand = new Random();
+        int random = rand.nextInt(number);
+        Log.i("random", random + "");
+        return random;
+    }
+
+    private static String randomTileColor()
+    {
+        Random rand = new Random();
+        return materialPalette[rand.nextInt(materialPalette.length)];
+
+    }
+
+}
